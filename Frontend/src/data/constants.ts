57f36e2d4d4877b2a956afabe615c89d2674a5db
Damian Sikora <me@imienazwisko.com>
2025-02-18 10:42:54 +0100

    Welcome view rough version (To be continued)

---
+++ Frontend/src/data/constants.ts
@@ -0,0 +1,13 @@
+import lang from "./pl.json"
+
+export class WelcomeText{
+    static AppName = "HUNOTE"
+    static Invitation = lang.welcome.invitation
+    static Welcome = lang.welcome.welcome
+    static LoginMessage = lang.welcome.loginMessage
+    static Email = lang.welcome.email
+    static Password = lang.welcome.password
+    static Login = lang.welcome.login
+    static Register = lang.welcome.register
+    static Google = lang.welcome.google
+}
